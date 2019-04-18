a42982fe1818607c6c63c4af405034c848427ebd
Symon <symon.nestoruk@polymtl.ca>
2019-04-18 14:01:51 -0400

    tp6 done :)

---
+++ TP6/fichiersFournis/version_eleves/Commande.cpp
@@ -0,0 +1,33 @@
+#include "Commande.h"
+
+/*
+* Date : 3 avril 2019
+* Auteur : Pierre-Olivier Côté
+*/
+
+Commande::Commande() {
+
+}
+QStringList Commande::getCommande() {
+	return commande_;
+}
+void Commande::ajouterPlat(QString nomPlat) {
+    commande_.push_back(nomPlat);
+    emit commandeModifie();
+}
+
+void Commande::retirerPlat(QString nomPlat) {
+    if(commande_.removeOne(nomPlat)){
+        emit commandeModifie();
+    }
+    //TODO OK :)
+    else throw ErreurPlatIntrouvable(LABEL_MEAL_NOT_FOUND);
+}
+
+void Commande::viderCommande(){
+    if(commande_.size() == 0){
+        throw ErreurCommandeVide(LABEL_NO_MEAL);
+    }
+    commande_.clear();
+    emit commandeModifie();
+}
---
+++ TP6/fichiersFournis/version_eleves/main.cpp
@@ -0,0 +1,36 @@
+#include "mainwindow.h"
+#include "Menu.h"
+#include "Filtre.h"
+#include "Commande.h"
+#include <QString>
+#include <QApplication>
+#include "erreurfichierintrouvable.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QString chemin = "C:\\Users\\Symon\\Documents\\INF1010\\TP6\\fichiersFournis" ; // INSERER CHEMIN ABSOLU VERS polyFood.txt
+    try {
+        Menu menu = Menu(chemin+FICHIER_POLYFOOD);
+        Filtre filtre(&menu);
+        Commande commande;
+        MainWindow mainGui(&commande, &filtre, &menu);
+
+
+        //TODO
+        //Connecter les mises a jour de la vue en fonction du modele (plats filtres + plats commande + prix)
+        QObject::connect(&filtre, &Filtre::choixFiltrageModifie, &mainGui, &MainWindow::mettreAJourPlatsFiltres);
+        QObject::connect(&commande, &Commande::commandeModifie, &mainGui, &MainWindow::mettreAJourPlatsCommande);
+        QObject::connect(&commande, &Commande::commandeModifie, &mainGui, &MainWindow::mettreAJourPrix);
+
+        mainGui.show();
+        return a.exec();
+
+    } catch (ErreurFichierIntrouvable erreur) {
+        QMessageBox mbox;
+        mbox.setText(erreur.what());
+        mbox.show();
+        return a.exec();
+    }
+
+}
